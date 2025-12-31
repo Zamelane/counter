086e7686fb5da6fba9888b950e0e1ba8023c1a5a
Zamelane <zamelane@vk.com>
2026-01-01 02:24:01 +0700

    Clear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter_Tight } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const interSans = Inter_Tight({
   variable: "--font-geist-sans",
@@ -26,12 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <Head>
-        <meta property="og:title" content="Коплю на квартиру" />
-        <meta property="og:description" content="Очень коплю..." />
-        <meta property="og:image" content="https://h.zmln.ru/og.png" />
-        <meta property="og:type" content="website" />
-      </Head>
       <body
         className={`${interSans.variable} antialiased`}
       >
